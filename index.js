945fdef154f165906d0662113a0f00e5f7c262b8
musta7188 <musta7188@hotmail.it>
2020-03-12 18:20:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-// Add your code here
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify({
+//         dogName: "Byron",
+//         dogBreed: "Poodle"
+//     })
+// };
+
+debugger
+//     fetch("http://localhost:3000/dogs", configurationObject)
+//         .then(function(response) {
+//             return response.json();
+//         })
+//         .then(function(object) {
+//             console.log(object);
+//         });
+// document.addEventListener('DOMContentLoaded', function() {
+//     
+// })
+
+
+
+function appendObj(obj) {
+    document.body.innerHTML = obj
+}
+
+
+
+function submitData(name, email) {
+    debugger
+    const urls = "http://localhost:3000/users"
+
+    const confObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+
+        body: JSON.stringify({
+            name,
+            email
+        })
+
+    }
+    return fetch(urls, confObj)
+        .then(response => response.json())
+        .then(obj => appendObj(obj["id"]))
+        .catch(error => appendObj(error))
+}
+
+
+
+
+// A body with the name and email passed in as arguments to submitData. 
+// These should be assigned to name and email keys within an object. This object should then be stringified
